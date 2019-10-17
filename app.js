78b2e02e883c902f712f526c6c7f292eabf63140
Rokas Vaitkevicius <rokas.vaitkevicius1@gmail.com>
2019-10-17 19:24:35 +0100

    Fixed overlapping for smaller screens, added typewriter for about me section

--- app.js
+++ app.js
@@ -38,30 +38,29 @@ new fullpage("#fullpage", {
     .fromTo(photo, 1, { height: "0%" }, { height: "40%", ease: Power2.easeInOut })
     .fromTo(pointing, 0.25, {y: "80%", opacity: 0}, {y: "50%", opacity: 1})
       } else if (destination.index === 2) {
-          tl.fromTo(other, 1, { y: "40%", opacity: 0 }, { y: "-20%", opacity: 1 })
-          .fromTo(repaythedelay, 1.5, { x: "-100%", y: "45%", opacity: 0}, {x: "-25%", y: "-45%", opacity: 1})
-          .fromTo(venuemenu, 1.5, {x: "75%", y: "45%", opacity: 0}, {x: "-75%", y: "-45%", opacity: 1})
+          tl.fromTo(other, 1, { y: "40%", opacity: 0 }, { y: "-30%", opacity: 1 })
+          .fromTo(repaythedelay, 1.5, { x: "-100%", y: "45%", opacity: 0}, {x: "-40%", y: "-45%", opacity: 1})
+          .fromTo(venuemenu, 1.5, {x: "75%", y: "45%", opacity: 0}, {x: "-58%", y: "-45%", opacity: 1})
           .fromTo(pointing, 0.25, {y: "80%", opacity: 0}, {y: "50%", opacity: 1})
 
       } else if (destination.index === 3) {
-          tl.fromTo(about, 1, { x: "50%", y: "20%", opacity: 0 }, { x: "-50%", y: "-20%", opacity: 1 })
-          .fromTo(paragraph[0], 3, { opacity: 0}, { opacity: 1})
-          .fromTo(paragraph[1], 3, { opacity: 0}, { opacity: 1})
-          .fromTo(paragraph[2], 3, { opacity: 0}, { opacity: 1})
-          .fromTo(paragraph[3], 3, { opacity: 0}, { opacity: 1})
-          .fromTo(paragraph[4], 3, { opacity: 0}, { opacity: 1})
-          .fromTo(paragraph[5], 3, { opacity: 0}, { opacity: 1})
-          .fromTo(paragraph[6], 3, { opacity: 0}, { opacity: 1})
-          .fromTo(paragraph[7], 3, { opacity: 0}, { opacity: 1})
-          .fromTo(paragraph[8], 3, { opacity: 0}, { opacity: 1})
-          .fromTo(paragraph[9], 3, { opacity: 0}, { opacity: 1})
-          .fromTo(paragraph[10], 3, { opacity: 0}, { opacity: 1})
-          .fromTo(pointing, 0.25, {y: "80%", opacity: 0}, {y: "50%", opacity: 1})
+          let pos = 0;
+          const speed = 35;
+          const str = "It's time for me to properly introduce myself, so Hi again! My name is Rokas and I am from Lithuania 🇱🇹. I was born in the capital city of Lithuania - Vilnius. I lived in Vilnius for 19 years before deciding on moving to London. In my early years I used to love spending my summer days playing football and basketball with my friends, but as the time passed some of them flew oversees while others grew older and found other interests. That lead to me spending more and more time on my own, and for me the best way to spend that time alone, was playing video games. Growing up I used to be the shyest kid ever, but recently I have started to gain more confidence. Although I am still a quite shy, awkward, nervous and scared of life, nowadays I tend to feel a lot more free and a lot more willing to try new things. Which is probably why I am embarking on this new programming journey. Generally I like to think of myself as relaxed, calm and easy going person, but I also love competition, so I tend to try my best and give my all in everything I do!"
+
+          function type() {
+            if (pos<str.length) {
+              document.querySelector(`.text-position`).innerHTML += str.charAt(pos);
+              pos++;
+              setTimeout(type, speed)
+            }
+          }
+          type()
       } else if (destination.index === 4) {
           tl.fromTo(title, 1, { y: "40%", opacity: 0 }, { y: "-20%", opacity: 1 })
           .fromTo(paragraph, 1, { opacity: 0}, { opacity: 1})
-          .fromTo(youtube, 0.5, { x: "-100%", y: "-70%", opacity: 0}, {x: "-30%", y: "-70%", opacity: 1})
-          .fromTo(spotify, 0.5, {x: "75%", y: "-70%", opacity: 0}, {x: "-60%", y: "-70%", opacity: 1})
+          .fromTo(youtube, 0.5, { x: "-100%", y: "-70%", opacity: 0}, {x: "-60%", y: "-60%", opacity: 1})
+          .fromTo(spotify, 0.5, {x: "75%", y: "-70%", opacity: 0}, {x: "0%", y: "-60%", opacity: 1})
           .fromTo(pointing, 0.25, {y: "80%", opacity: 0}, {y: "50%", opacity: 1})
         } else if (destination.index === 5) {
           tl.fromTo(about, 1, { y: "40%", opacity: 0 }, { y: "-20%", opacity: 1 })
@@ -73,3 +72,7 @@ new fullpage("#fullpage", {
         }
   }
 });
+
+
+
+
